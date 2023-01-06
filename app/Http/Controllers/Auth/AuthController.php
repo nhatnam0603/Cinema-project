f46f99a22518476432c21f381634cd56c669fc43
maitrieuchinh <chincholamu@gmail.com>
2023-01-06 20:05:42 +0700

    Thêm page dashboard vào admin

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,14 +1,14 @@
 <?php
-  
+
 namespace App\Http\Controllers\Auth;
-  
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Session;
 use App\Models\User;
 use Hash;
-  
+
 class AuthController extends Controller
 {
     /**
@@ -19,8 +19,8 @@ class AuthController extends Controller
     public function index()
     {
         return view('auth.login');
-    }  
-      
+    }
+
     /**
      * Write code on Method
      *
@@ -30,7 +30,7 @@ class AuthController extends Controller
     {
         return view('auth.registration');
     }
-      
+
     /**
      * Write code on Method
      *
@@ -42,35 +42,35 @@ class AuthController extends Controller
             'email' => 'required',
             'password' => 'required',
         ]);
-   
+
         $credentials = $request->only('email', 'password');
         if (Auth::attempt($credentials)) {
             return redirect()->intended('dashboard')
                         ->withSuccess('You have Successfully loggedin');
         }
-  
+
         return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
     }
-      
+
     /**
      * Write code on Method
      *
      * @return response()
      */
     public function postRegistration(Request $request)
-    {  
+    {
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:6',
         ]);
-           
+
         $data = $request->all();
         $check = $this->create($data);
-         
+
         return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
     }
-    
+
     /**
      * Write code on Method
      *
@@ -79,12 +79,12 @@ class AuthController extends Controller
     public function dashboard()
     {
         if(Auth::check()){
-            return view('dashboard');
+            return view('admin.dashboard');
         }
-  
+
         return redirect("login")->withSuccess('Opps! You do not have access');
     }
-    
+
     /**
      * Write code on Method
      *
@@ -98,7 +98,7 @@ class AuthController extends Controller
         'password' => Hash::make($data['password'])
       ]);
     }
-    
+
     /**
      * Write code on Method
      *
@@ -107,7 +107,7 @@ class AuthController extends Controller
     public function logout() {
         Session::flush();
         Auth::logout();
-  
+
         return Redirect('login');
     }
 }
