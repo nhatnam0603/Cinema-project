dd9dafca124da7771722a07b42d1d0593f000535
Nam Nhat Doan <nam.doan@ilavietnam.com>
2023-02-17 16:30:17 +0700

    fix link to website from dashboard navbar

--- resources/views/layout/inc/admin/navbar.blade.php
+++ resources/views/layout/inc/admin/navbar.blade.php
@@ -2,8 +2,8 @@
     <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="navbar-brand-wrapper d-flex justify-content-center">
             <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-                <a class="navbar-brand brand-logo" href="index.html"><img src="{{ asset('admin/images/logo.svg') }}" alt="logo" /></a>
-                <a class="navbar-brand brand-logo-mini" href="index.html"><img src="{{ asset('admin/images/logo-mini.svg')}}"
+                <a class="navbar-brand brand-logo" href="{{route('home')}}"><img src="{{ asset('assets/img/logo/logo.png') }}" alt="logo" /></a>
+                <a class="navbar-brand brand-logo-mini" href="{{route('home')}}"><img src="{{ asset('assets/img/logo/logo.png')}}"
                         alt="logo" /></a>
                 <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
                     <span class="mdi mdi-sort-variant"></span>
@@ -15,7 +15,7 @@
                 <li class="nav-item dropdown me-4">
                 <li class="nav-item nav-profile dropdown">
                     <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-                        <img src="{{ asset('admin/images/faces/face5.jpg') }}" alt="profile" />
+                        <!-- <img src="{{ asset('admin/images/faces/face5.jpg') }}" alt="profile" /> -->
                         <span class="nav-profile-name">
                                 @if(auth()->check())
                                 <span>{{auth()->user()->name}}</span>
